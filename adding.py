c1fd3eb6b299591bf1b07e07aaeb1a390e1681e2
X00360200 <47256597+X00360200@users.noreply.github.com>
2020-03-10 11:50:26 -0500

    Added product and quotient

--- adding.py
+++ adding.py
@@ -8,3 +8,11 @@ print('the sum is', the_result)
 the_result = num1 - num2
 
 print('the difference is', the_result)
+
+the_result = num1 * num2
+
+print('the product is', the_result)
+
+the_result = num1 / num2
+
+print('the quotient is', the_result)
